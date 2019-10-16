b664f4f1c0c23bb9c0e36c8028f59127860f965e
Pikkewynn <chara.grant@gmail.com>
2019-10-16 19:11:37 +0200

    Completed command structure skeleton in comments - still to be tested incrementally

--- AP_Server_ArduinoFramework/src/main.cpp
+++ AP_Server_ArduinoFramework/src/main.cpp
@@ -2,7 +2,9 @@
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 #include <WiFiAP.h>
-#include <string.h>
+#include <HardwareSerial.h>
+#include <pins_arduino.h>
+
 
 const char * ssid = "ESPAP";
 const char * password = "hellodaar";
@@ -13,8 +15,45 @@ String mode, fire; //placeholder for "led" in absence of breadboard and such
 int tilt = 0;
 int pan = 0;
 
+//destinations: PANT, FARM - the destination names for the pan&tilt and fire&arm mechanisms
+
 AsyncWebServer server(80);
 
+
+//Serial port definitions
+//HardwareSerial Serial1(1); //declare first serial communication set - PANT
+//HardwareSerial Serial2(2); //declare second serial communication set - FARM
+
+//definition of TX/RX pins for serial communication
+//#define RX1 4
+//#define TX1 2
+//#define RX2 16
+//#define TX2 17
+
+//Command manager
+
+/*void command(String destination, String command, String value)
+{
+  switch (destination){
+    case "PANT":
+    {
+      Serial1.print(command);   //print command to PANT
+      Serial1.println(value);   //print value to PANT
+      break;
+    }
+    case "FARM":
+    {
+      Serial2.print(command);   //print command to FARM
+      Serial2.println(value);   //print value to FARM
+      break;
+    }
+    default:
+    {
+      Serial.println("Destination not provided");
+      break;
+    }
+  };
+}*/
 
 String processor(const String& var)  //html string variable processor - will be understood later.
 {
@@ -58,7 +97,10 @@ String processor(const String& var)  //html string variable processor - will be 
 
 void setup() {
   // put your setup code here, to run once:
+  //Set up Serial Communications:
   Serial.begin(115200);
+  //Serial1.begin(9600, SERIAL_8N1, RX1, TX1); //begin communication with PANT at 9600 baud
+  //Serial2.begin(9600, SERIAL_8N1, RX2, TX2); //begin communication with FARM at 9600 baud
   mode = "OFF";
   fire = "NO";
   tiltdeg = "0";
@@ -69,7 +111,7 @@ void setup() {
   return;
   }
   WiFi.softAP(ssid, password);
-  IPAddress IP = WiFi.softAPIP();
+  //IPAddress IP = WiFi.softAPIP();
   
   //Serial.println(IP);
   //path to html file for GET requests (html):
@@ -96,6 +138,7 @@ void setup() {
       }
        //turn this into a toggle?   
     request->send(SPIFFS, "/index.html", String(), false, processor);
+    //command("FARM","ARM",mode);
     Serial.println(mode);
   });
 
@@ -112,6 +155,7 @@ void setup() {
     else
     {
       fire = "YES";
+      //command("FARM","FIRE",fire);
        Serial.println(fire);
     }
       //make this the fire button
@@ -120,46 +164,61 @@ void setup() {
 
 //http get request setup for pan and tilt buttons
   server.on("/up", HTTP_GET, [](AsyncWebServerRequest *request){
-    if (tilt < 90)
+    if ((mode != "ON") && (tilt < 90))
     {
       tilt+=1;
+      tiltdeg = String(tilt);
+      //command("PANT","TILT",tiltdeg);
+      Serial.println(tiltdeg);
     }
     request->send(SPIFFS, "/index.html", String(), false, processor);
-    Serial.println(tiltdeg);
+    
   });
 
   server.on("/down", HTTP_GET, [](AsyncWebServerRequest *request){
-    if (tilt > 0)
+    if ((mode != "ON")&&(tilt > 0))
     {
       tilt-=1;
+      tiltdeg = String(tilt);
+      //command("PANT","TILT",tiltdeg);
+      Serial.println(tiltdeg);
     }
     request->send(SPIFFS, "/index.html", String(), false, processor);
-    Serial.println(tiltdeg);
   });
 
   server.on("/left", HTTP_GET, [](AsyncWebServerRequest *request){
-    if (pan > -60)
+    if ((mode!="ON")&&(pan > -60))
     {
       pan-=1;
       pandeg = String(pan);
+      //command("PANT","PAN",pandeg);
       Serial.println(pandeg);
     }
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
   server.on("/right", HTTP_GET, [](AsyncWebServerRequest *request){
-    if (pan < 60)
+    if ((mode!="ON")&&(pan < 60))
     {
       pan+=1;
       pandeg = String(pan);
+      //command("PANT","PAN",pandeg);
       Serial.println(pandeg);
     }
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
   
   server.on("/zero", HTTP_GET, [](AsyncWebServerRequest *request){
+    if (mode != "ON")
+    {
+     tilt = 0;
     pan = 0;
     pandeg = String(pan);
-    Serial.println(pandeg);
+    tiltdeg = String(tilt);
+    //command("PANT","TILT",tiltdeg);
+    //command("PANT","PAN",pandeg);
+    Serial.println(tiltdeg);
+    Serial.println(pandeg); 
+    }
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
   server.begin();
